3c55b5f708a119a5bf9f5b0134d41cf39b1ba472
nhhaiit <nguyenhai4072@gmail.com>
2022-04-25 14:46:51 +0700

    update express

---
+++ routes/users.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/hello', function(req, res, next){
+  res.send('Hello world');
+});
+module.exports = router;
